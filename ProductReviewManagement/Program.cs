ecc11749b50f016c05649aa34b5ea0c441b8279f
Snehil <snehilverma488@gmail.com>
2020-11-06 20:18:52 +0530

    [Snehil] Add : Creating Product Review Model Class

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProducID=1,UserID=1,Rating=2,Review="Good",IsLike=true},
+                new ProductReview(){ProducID=2,UserID=1,Rating=4,Review="Good",IsLike=true},
+                new ProductReview(){ProducID=3,UserID=1,Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProducID=4,UserID=1,Rating=6,Review="Good",IsLike=false},
+                new ProductReview(){ProducID=5,UserID=1,Rating=2,Review="nice",IsLike=true},
+                new ProductReview(){ProducID=6,UserID=1,Rating=1,Review="bad",IsLike=true},
+                new ProductReview(){ProducID=8,UserID=1,Rating=1,Review="Good",IsLike=false},
+                new ProductReview(){ProducID=8,UserID=1,Rating=9,Review="nice",IsLike=true},
+                new ProductReview(){ProducID=2,UserID=1,Rating=10,Review="nice",IsLike=true},
+                new ProductReview(){ProducID=10,UserID=1,Rating=8,Review="nice",IsLike=true},
+                new ProductReview(){ProducID=11,UserID=1,Rating=3,Review="nice",IsLike=true}
+            };
+        }
+    }
+}
